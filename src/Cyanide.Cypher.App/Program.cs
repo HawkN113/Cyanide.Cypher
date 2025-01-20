b52f04c2084afbb14791e603b3f63d48cc39d2a1
Nikolay Selyutin <nikolay.selyutin@lamie.io>
2025-01-20 11:01:21 +0100

    Updated jon settings

--- src/Cyanide.Cypher.App/Program.cs
+++ src/Cyanide.Cypher.App/Program.cs
@@ -1,10 +1,20 @@
-﻿using BenchmarkDotNet.Configs;
+﻿using BenchmarkDotNet.Analysers;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Running;
 using Cyanide.Cypher.App;
 
-var config = DefaultConfig.Instance.WithOptions(ConfigOptions.DisableOptimizationsValidator);
+var config = DefaultConfig.Instance
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .AddJob(Job.Dry)
+    .AddLogger(ConsoleLogger.Default)
+    .AddColumn(TargetMethodColumn.Method, StatisticColumn.Min, StatisticColumn.Max)
+    .AddExporter(RPlotExporter.Default, CsvExporter.Default)
+    .AddAnalyser(EnvironmentAnalyser.Default);
 
 BenchmarkRunner.Run<QueriesManipulationBenchmarks>(config);
 BenchmarkRunner.Run<AdminQueriesManipulationBenchmarks>(config);
-
-Console.ReadKey();
